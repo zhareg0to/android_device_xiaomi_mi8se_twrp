13191fa1bfba7bc8a7826e0869bf9b348a0d84c6
mitushiro.hirayama <m.hirayam@mizhilow.com>
2018-07-31 11:59:39 +0900

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,7 @@ BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
 
